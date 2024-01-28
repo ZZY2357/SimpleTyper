2d8e4178aac9b1abb63b6970efc89d8933c7d356
ZZY2357 <47471318+ZZY2357@users.noreply.github.com>
2024-01-28 23:07:22 +0800

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+let chars = "_← ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let index = 0;
+let text = "";
+let lastTypeTime = Date.now();
+
+$(() => {
+    $(window).on("keypress", () => {
+        lastTypeTime = Date.now();
+        index = (index + 1) % chars.length;
+        $("#tip").text(chars[index]);
+    });
+    setInterval(() => {
+        if (Date.now() - lastTypeTime > 500) {
+            if (chars[index] != "_") {
+                if (chars[index] === "←") {
+                    if (text.length > 0) {
+                        text = text.slice(0, text.length - 1);
+                    }
+                } else {
+                    text += chars[index];
+                }
+                if (text.length > 0) {
+                    $("#display").text(text);
+                } else {
+                    $("#display").text("Press any key to type.");
+                }
+            }
+            index = 0;
+            $("#tip").text(chars[index]);
+            lastTypeTime = Date.now();
+        }
+    }, 30);
+});
